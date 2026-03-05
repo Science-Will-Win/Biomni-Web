0e090f36bb3d3f52d708c1220fa1d1e0daf71a58
ZN-Sellena47 <jiyeonl2000@naver.com>
2026-03-05 11:20:25 +0900

    build: seperate Dockerfile into base for installing biomni tools and backend for biomni-web

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -4,6 +4,6 @@ opentelemetry-instrument \
     --metrics_exporter otlp \
     --logs_exporter otlp \
     --service_name biomni-backend \
-    --exporter_otlp_endpoint "http://34.47.127.84:4317" \
+    --exporter_otlp_endpoint "http://172.17.0.1:4317" \
     --exporter_otlp_protocol "grpc" \
     uvicorn main:app --host 0.0.0.0 --port 8000 --reload
